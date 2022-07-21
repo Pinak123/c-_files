fa741514613041b4f097e1338568134239fc3c91
Pinak <sndipsarode1155@gmail.com>
2022-07-21 21:42:56 +0530

    somethin in virtual base class

--- 36th-virtusl_base.cpp
+++ 36th-virtusl_base.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
-
+class students {
+    protected:
+        int roll_no;
+    public:
+        void setnum(int a)
+        {
+            roll_no =a;
+        }
+        void print_num(void )
+        {
+            cout<<"you roll number is "<<roll_no<<endl; 
+        }
+};
 int main() {
     
     return 0;
