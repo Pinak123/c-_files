f7657e8ec395fbf9669448ed91d9738ac3142d15
Pinak <sndipsarode1155@gmail.com>
2022-07-27 14:23:47 +0530

    just committing in a while

--- 36th-virtusl_base.cpp
+++ 36th-virtusl_base.cpp
@@ -10,10 +10,60 @@ class students {
         }
         void print_num(void )
         {
-            cout<<"you roll number is "<<roll_no<<endl; 
+            cout<<"you roll number is "<<roll_no<<endl<<endl; 
+        }
+};
+
+//Creating test from stuudent
+class test : virtual public students{
+    protected:
+        float maths, physics;
+    public:
+        void set_marks(float a, float b)
+        {
+            maths  = a;
+            physics = b;
+        }
+        void print_marks(void )
+        {
+            cout <<"your marks obtained are :-"<<endl
+                 <<"maths: "<<maths<<endl
+                 <<"physics: "<<physics<<endl<<endl;
+        }
+};
+class sports : virtual public students{
+    protected:
+        float scores;
+    public:
+        void set_scores(float sc)
+        {
+            scores= sc;
+        }
+        void print_scores(void )
+        {
+            cout<<"your pt score is :- "<<scores<<endl<<endl;
+        }
+};
+
+class result : public test , public sports{
+    private:
+        float total;
+    public:
+        void display(void)
+        {
+            total= maths + physics + scores;
+            print_num();
+            print_marks();
+            print_scores();
+            cout<<"your total score is "<<endl 
+                <<total<<endl;
         }
 };
 int main() {
-    
+    result Pinak;
+    Pinak.set_marks(100 , 100);
+    Pinak.setnum(22);
+    Pinak.set_scores(99);
+    Pinak.display();
     return 0;
 }
