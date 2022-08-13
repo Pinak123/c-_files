5d86ef229b7b6e5d784cef1e194384b374d8269a
Pinak <sndipsarode1155@gmail.com>
2022-08-13 19:20:46 +0530

    learnt about polymorphism

---
+++ 43rd-polymorphism.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+/*
+  Polymorphism
+  -one name but multiple jobs
+  -eg; function overloading
+
+  There are two types of polymorphism 
+  1.compile time polymorphism --> acheived using function overloading and operator overloading
+  2.Runtime polymorphism --> acheived usibg virtual functions
+*/
+int main() {
+    
+    return 0;
+}
